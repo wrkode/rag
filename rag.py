e2c488825ec2eeae17d8b5d1c25e00d960399d51
wrkode <william.rizzo@suse.com>
2023-05-31 09:36:04 +0200

    added logic for inactive clusters

--- rag.py
+++ rag.py
@@ -39,25 +39,26 @@ def main():
     
     # Loop over clusters
     for cluster in clusters:
-        # Check if cluster is active
-        if cluster['state'] != 'active':
-            continue
-        
         # Initialize table
         cluster_table = PrettyTable(['Node Name', 'Node IP'])
-        print("Cluster Name: %s, Cluster ID: %s, Kubernetes Version: %s, Provider: %s" % 
+        
+        kubernetes_version = cluster['version']['gitVersion'] if 'version' in cluster and 'gitVersion' in cluster['version'] else 'N/A'
+        
+        print("Cluster Name: %s, Cluster ID: %s, Kubernetes Version: %s, Provider: %s, State: %s" % 
               (colored(cluster['name'], 'red'), 
                colored(cluster['id'], 'green'), 
-               colored(cluster['version']['gitVersion'], 'yellow'), 
-               colored(cluster['provider'], 'blue')))
+               colored(kubernetes_version, 'yellow'), 
+               colored(cluster['provider'], 'blue'),
+               colored(cluster['state'], 'magenta')))
 
-        # Get nodes for each cluster
-        response = requests.get(rancher_server_url + '/v3/nodes', headers=headers, params={'clusterId': cluster['id']}, verify=False)
-        nodes = json.loads(response.text)['data']
-        
-        # Loop over nodes and add them to table
-        for node in nodes:
-            cluster_table.add_row([node['hostname'], node['ipAddress']])
+        # Get nodes for each cluster if it is in active state
+        if cluster['state'] == 'active':
+            response = requests.get(rancher_server_url + '/v3/nodes', headers=headers, params={'clusterId': cluster['id']}, verify=False)
+            nodes = json.loads(response.text)['data']
+            
+            # Loop over nodes and add them to table
+            for node in nodes:
+                cluster_table.add_row([node['hostname'], node['ipAddress']])
 
         # Print table
         print(cluster_table)
